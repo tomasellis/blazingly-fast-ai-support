0971115edb4378f316d6dc7ed4ea7353bf6a8ff4
Tomas Ellis <tomas.sydney.ellis@gmail.com>
2024-01-07 11:32:06 -0300

    fix

--- components/providers.tsx
+++ components/providers.tsx
@@ -4,7 +4,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import * as React from "react";
-import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
 
 export function Providers(props: { children: React.ReactNode }) {
   const [queryClient] = React.useState(
@@ -20,9 +19,7 @@ export function Providers(props: { children: React.ReactNode }) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryStreamedHydration>
-        {props.children}
-      </ReactQueryStreamedHydration>
+      {props.children}
       {<ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   );
