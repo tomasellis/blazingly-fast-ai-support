fa51f8b539fc226ee21af8ac80c8638baa58d520
Tomas Ellis <tomas.sydney.ellis@gmail.com>
2024-01-23 21:02:12 -0300

    fix: redirect to /tickets when reaching /

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
+  redirect("/tickets");
+  /* return (
     <div className="font-black text-9xl">
       Nothing to see here chief, go to{" "}
       <Link className="text-blue-500" href={"/tickets"}>
         TICKETS
       </Link>
     </div>
-  );
+  ); */
 }
